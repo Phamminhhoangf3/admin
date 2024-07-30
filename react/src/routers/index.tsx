c9be3594488355bd1be0995ead7dc0ff0a43fa37
pham minh hoang <hoang.pm@softworld.jp>
2024-07-30 17:27:55 +0700

    add view user

--- react/src/routers/index.tsx
+++ react/src/routers/index.tsx
@@ -2,7 +2,7 @@ import { useRoutes } from "react-router-dom";
 import Home from "~/pages/home";
 import MainLayout from "~/components/layout/main-layout";
 import Users from "~/pages/users";
-import DetailUser from "~/pages/users/add";
+import DetailUser from "~/pages/users/detail";
 import { paths } from "~/constants/path";
 
 export default function RouteElements() {
@@ -16,7 +16,14 @@ export default function RouteElements() {
           path: paths.users.substring(1),
           children: [
             { path: "", element: <Users /> },
-            { path: "add", element: <DetailUser typePage="add" /> },
+            {
+              path: paths.addUser.split("/")[2],
+              element: <DetailUser typePage="add" />,
+            },
+            {
+              path: 'view/:id',
+              element: <DetailUser typePage="detail" />,
+            },
           ],
         },
       ],
