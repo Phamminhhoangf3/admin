5dbd26d828604552a68ee8573f2c82a96ad1b12f
pham minh hoang <hoang.pm@softworld.jp>
2024-07-31 14:38:16 +0700

    fix cors, add firebase cli backend

--- nodejs/src/config/cors.js
+++ nodejs/src/config/cors.js
@@ -1,25 +1,25 @@
-import { WHITELIST_DOMAINS } from '~/utils/constants'
-import { env } from '~/config/environment'
-import { StatusCodes } from 'http-status-codes'
-import ApiError from '~/utils/ApiError'
+import { WHITELIST_DOMAINS } from '~/utils/constants';
+import { env } from '~/config/environment';
+import { StatusCodes } from 'http-status-codes';
+import ApiError from '~/utils/ApiError';
 
 // Cấu hình CORS Option trong dự án thực tế (Video số 62 trong chuỗi MERN Stack Pro)
 export const corsOptions = {
   origin: function (origin, callback) {
     // nếu môi trường là dev thì cho qua
     if (env.BUILD_MODE === 'dev') {
-      return callback(null, true)
+      return callback(null, true);
     }
 
     // Kiểm tra xem origin có phải là domain được chấp nhận hay không
-    if (WHITELIST_DOMAINS.includes(origin)) {
-      return callback(null, true)
+    if (!origin || WHITELIST_DOMAINS.includes(origin)) {
+      return callback(null, true);
     }
 
     // Cuối cùng nếu domain không được chấp nhận thì trả về lỗi
     return callback(
       new ApiError(StatusCodes.FORBIDDEN, `${origin} not allowed by our CORS Policy.`)
-    )
+    );
   },
 
   // Some legacy browsers (IE11, various SmartTVs) choke on 204
@@ -27,4 +27,4 @@ export const corsOptions = {
 
   // CORS sẽ cho phép nhận cookies từ request, (Nhá hàng :D | Ở khóa MERN Stack Advance nâng cao học trực tiếp mình sẽ hướng dẫn các bạn đính kèm jwt access token và refresh token vào httpOnly Cookies)
   credentials: true
-}
+};
